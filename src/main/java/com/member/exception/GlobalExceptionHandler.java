22b7ba8f813fbf8c548a9c4df7bffc9b40dd5565
royhsle0120 <royhslee0120@gmail.com>
2026-02-03 15:09:24 +0900

    fix : 지속된 오류 수정

--- src/main/java/com/member/exception/GlobalExceptionHandler.java
+++ src/main/java/com/member/exception/GlobalExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.member.controller")
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
